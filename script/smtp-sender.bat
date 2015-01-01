7d94dc14f28b531049fbb2116bb02ccf46ef5792
inter6 <inter6@naver.com>
2015-01-02 08:28:31 +0900

    윈도우 실행 스크립트 추가

---
+++ script/smtp-sender.bat
@@ -0,0 +1,9 @@
+SET APP_HOME=D:\opt\smtp-sender
+SET APP_NAME=smtp-sender
+SET APP_CONFIG=%APP_HOME%/config/default.config
+SET DEFAULT_ENCODING=utf-8
+
+SET JAVA=D:\java\sdk\jdk6\bin\java
+SET JAVA_OPTION=-server -Xms128M -Xmx256M -Dibm.stream.nio=true
+
+%JAVA% -jar %JAVA_OPTION% -Djob=%APP_NAME% -Dfile.encoding=%DEFAULT_ENCODING% -Dapp.config=%APP_CONFIG% -Dlogback.configurationFile=%APP_HOME%/config/logback.xml %APP_HOME%/smtp-sender.jar
